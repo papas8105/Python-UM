ef8c10e28643a6b681c4e51bad1901316658b231
papas8105 <pgeorgios8@gmail.com>
2016-07-01 15:38:41 +0300

    added 1/2 ass week 12b

---
+++ Chapters [11-13]/chapter 12/solutions-week12b-1.py
@@ -0,0 +1,26 @@
+'''
+You need to adjust this code to look for span tags and pull out the text content of the span tag, convert them to integers and add them up to complete the assignment. 
+'''
+import urllib
+from BeautifulSoup import *
+url = raw_input('Enter - ')
+if len(url)<3:
+	url = 'http://python-data.dr-chuck.net/comments_288507.html'
+html = urllib.urlopen(url).read()
+soup = BeautifulSoup(html)
+
+# Retrieve all of the anchor tags
+tags = soup('span')
+for tag in tags:
+    # Look at the parts of a tag
+    print 'TAG:',tag
+    print 'URL:',tag.get('href', None)
+    print 'Contents:',tag.contents[0]
+    print 'Attrs:',tag.attrs
+    print '\n'
+
+s = 0
+for tag in tags:
+	x = int(tag.contents[0])
+	s = s + x
+print 'The total contents sum is: ' , s
