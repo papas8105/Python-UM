f187330b8eba04b47120e232b0b00967ba5bfea8
papas8105 <pgeorgios8@gmail.com>
2016-07-08 16:12:12 +0300

    ..

--- Chapters [14-16]/chapter 14/emaildb14b.py
+++ Chapters [14-16]/chapter 14/emaildb14b.py
@@ -35,7 +35,7 @@ cur.execute('''
 CREATE TABLE Counts (org TEXT, count INTEGER)''')
 
 fname = raw_input('Enter file name: ')
-if ( len(fname) < 1 ) : fname = 'mbox.txt'
+if len(fname) < 1: fname = 'mbox.txt'
 fh = open(fname)
 for line in fh:
     if not line.startswith('From ') : continue
