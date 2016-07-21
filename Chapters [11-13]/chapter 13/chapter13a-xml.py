e77d20bc37104cc0e21fac47031d65959f68b523
papas8105 <pgeorgios8@gmail.com>
2016-07-21 17:46:43 +0300

    geojson

--- Chapters [11-13]/chapter 13/chapter13a-xml.py
+++ Chapters [11-13]/chapter 13/chapter13a-xml.py
@@ -4,7 +4,7 @@ Welcome Georgios Papadopoulos from Using Python to Access Web Data
 Extracting Data from XML
 
 In this assignment you will write a Python program somewhat similar to
-http://www.pythonlearn.com/code/geoxml.py. The program will prompt for a URL, 
+http://www.python`-learn.com/code/geoxml.py. The program will prompt for a URL, 
 read the XML data from that URL using urllib and then parse and extract the comment counts from the XML data,
 compute the sum of the numbers in the file.
 
