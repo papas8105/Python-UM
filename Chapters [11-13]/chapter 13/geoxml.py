5241b2961e424e445cbe302f01969d272f1638b9
papas8105 <pgeorgios8@gmail.com>
2016-07-22 14:24:11 +0300

    minor changes

--- Chapters [11-13]/chapter 13/geoxml.py
+++ Chapters [11-13]/chapter 13/geoxml.py
@@ -1,12 +1,10 @@
 import urllib
 import xml.etree.ElementTree as ET
-
 serviceurl = 'http://maps.googleapis.com/maps/api/geocode/xml?'
 
 while True:
     address = raw_input('Enter location: ')
     if len(address) < 1 : break
-
     url = serviceurl + urllib.urlencode({'sensor':'false', 'address': address})
     print 'Retrieving', url
     uh = urllib.urlopen(url)
@@ -14,12 +12,9 @@ while True:
     print 'Retrieved',len(data),'characters'
     print data
     tree = ET.fromstring(data)
-
-
     results = tree.findall('result')
     lat = results[0].find('geometry').find('location').find('lat').text
     lng = results[0].find('geometry').find('location').find('lng').text
     location = results[0].find('formatted_address').text
-
     print 'lat',lat,'lng',lng
     print location
